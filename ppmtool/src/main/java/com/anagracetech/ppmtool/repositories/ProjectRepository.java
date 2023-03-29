69b67ed3b3145d52ad67f07b6a276e9cf2e745f8
Chima-Anason <tblynxx@gmail.com>
2023-03-30 06:52:54 +0800

    Setup Project Domain

---
+++ ppmtool/src/main/java/com/anagracetech/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,12 @@
+package com.anagracetech.ppmtool.repositories;
+
+import com.anagracetech.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> longs);
+}
