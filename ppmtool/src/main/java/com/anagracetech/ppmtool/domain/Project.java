912a19ebd0ff1f923ba2c6ca56e57b7caccca239
Chima-Anason <tblynxx@gmail.com>
2023-03-31 02:00:55 +0800

    Custom Exception for unique ProjectIdentifier. Closes #15

--- ppmtool/src/main/java/com/anagracetech/ppmtool/domain/Project.java
+++ ppmtool/src/main/java/com/anagracetech/ppmtool/domain/Project.java
@@ -72,7 +72,7 @@ public class Project {
     }
 
     public void setProjectIdentifier(String projectIdentifier) {
-        this.projectIdentifier = projectIdentifier;
+        this.projectIdentifier = projectIdentifier.toUpperCase();
     }
 
     public String getDescription() {
