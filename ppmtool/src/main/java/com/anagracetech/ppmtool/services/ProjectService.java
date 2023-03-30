4099d555712d137c10384f0afd1b45bb04919c4f
Chima-Anason <tblynxx@gmail.com>
2023-03-31 03:28:09 +0800

    Delete Project by projectId. Closes #21

--- ppmtool/src/main/java/com/anagracetech/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/com/anagracetech/ppmtool/services/ProjectService.java
@@ -35,4 +35,13 @@ public class ProjectService {
     }
 
     public Iterable<Project> getAllProjects(){return projectRepository.findAll();}
+
+    public void deleteProjectByProjectIdentifier(String projectIdentifier){
+        Project project = projectRepository.findByProjectIdentifier(projectIdentifier.toUpperCase());
+        if(project == null){
+            throw new ProjectIdException("Cannot delete Project with ID: '"+projectIdentifier+"'. This project does not exist!");
+        }
+
+        projectRepository.delete(project);
+    }
 }
